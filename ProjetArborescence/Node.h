be0c32baef53845bb21ffc65574a2a0737c5e2b1
ThomasPELLETIER <thms.pelletier@gmail.com>
2017-09-24 11:27:20 +0200

    Added NodeDesc

--- ProjetArborescence/Node.h
+++ ProjetArborescence/Node.h
@@ -1,12 +1,18 @@
 #pragma once
+#include <string>
+
 class Node
 {
 private:
 	static int compteur;
 	int id;
+	
+	std::string desc;
 
 public:
 	Node();
 	~Node();
+
+	std::string getDescription() const;
 };
 
